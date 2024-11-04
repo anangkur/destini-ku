f904a10f5931b0a9b010fbb98aa31a3701a749f3
Anang Kurniawan <anangk97@gmail.com>
2024-11-04 17:25:22 +0700

    Initial Commit

---
+++ destinikuTests/destinikuTests.swift
@@ -0,0 +1,17 @@
+//
+//  destinikuTests.swift
+//  destinikuTests
+//
+//  Created by Anang Kurniawan on 04/11/24.
+//
+
+import Testing
+@testable import destiniku
+
+struct destinikuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
